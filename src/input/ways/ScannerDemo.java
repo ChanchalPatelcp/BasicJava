19c777a3d0dc95afe589b97c440917ef44cd7734
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-11-10 16:51:41 +0530

    input

--- src/input/ways/ScannerDemo.java
+++ src/input/ways/ScannerDemo.java
@@ -1,10 +1,21 @@
 package input.ways;
-
+import java.util.Scanner;
 public class ScannerDemo {
 
 	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("Enter the information : ");
+		 int num = scan.nextInt();
+		 String name = scan.nextLine();
+		 String name1 = scan.nextLine();
+		 float num1 = scan.nextFloat();
+		 double num2 = scan.nextDouble();
+		 short s = scan.nextShort();
+		 boolean flag = scan.hasNext();
 		 
-
+		 System.out.println(num+" "+name1+" "+num1+" "+num2+" "+s+" "+flag);
+		 
+		 
 	}
 
 }
