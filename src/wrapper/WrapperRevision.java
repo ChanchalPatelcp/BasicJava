06b24c3240bdd0833fcfd12e57525b40881af8a3
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-04 21:09:25 +0530

    Wrapper

---
+++ src/wrapper/WrapperRevision.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class WrapperRevision {
+
+	public static void main(String[] args) {
+			Integer in = 2;
+			System.out.println(in.toString());
+			int a = 2;
+			Integer in1 = a;
+			System.out.println(in1);
+			
+
+	}
+
+}
