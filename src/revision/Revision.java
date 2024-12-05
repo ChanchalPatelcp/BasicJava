f12edd5c3a9a95a1439d8f09e30377aabe038499
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-05 22:15:30 +0530

    StringArray

---
+++ src/revision/Revision.java
@@ -0,0 +1,23 @@
+package revision;
+
+import java.util.Scanner;
+
+public class Revision {
+
+	public static void main(String[] args) {
+		arr();	
+
+	}
+	public static void arr() {
+		Scanner sc = new Scanner(System.in);
+		String arr[] = new String[100];
+		System.out.print("Enter a 10 word sentence : ");
+		for(int i=0;i<10;i++) {
+			arr[i] = sc.next();
+		}
+		for(int i=0;i<10;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+}
