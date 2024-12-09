45cf992e836670a2832fafb93e3a06f60d18e293
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-09 22:29:48 +0530

    Code

--- src/revision/LeetCode.java
+++ src/revision/LeetCode.java
@@ -3,8 +3,13 @@ package revision;
 public class LeetCode {
 
 	public static void main(String[] args) {
-		
+		LeetCode lc = new LeetCode();
+		lc.num();
 
+	}
+	public void num() {
+		System.out.println("Only For Git");
+		
 	}
 
 }
