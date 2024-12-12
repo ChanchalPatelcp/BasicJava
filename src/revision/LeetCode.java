546a119fa6d899f6a978b6e035fad9ff4a056179
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-12 22:06:14 +0530

    leet

--- src/revision/LeetCode.java
+++ src/revision/LeetCode.java
@@ -6,57 +6,43 @@ public class LeetCode {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-//		getData();
-//		sortingList();
-		deleteDuplicate();
+		plusOne();
+//		deleteDuplicate();
 
 	}
 
-	public static void getData() {
-
-		int[] list1 = new int[3];
-		int[] list2 = new int[3];
-
-		System.out.print("Enter List1 : ");
-		for (int i = 0; i < 3; i++) {
-			list1[i] = sc.nextInt();
-		}
-		System.out.print("Enter List2 : ");
-		for (int i = 0; i < 3; i++) {
-			list2[i] = sc.nextInt();
-		}
-
-	}
-
-	public static void sortingList() {
-		int[] list1 = new int[3];
-		int[] list2 = new int[3];
-		int k = 0;
-
-		for (int i = 1; i < 3; i++) {
-			if (list1[i] > list1[i - 1]) {
-				list1[k] = list1[i - 1];
-				k++;
-			}
-
-		}
-		System.out.println(list1[k]);
-
-	}
 	public static void deleteDuplicate() {
-		int[] list = {1,1,2,2,3,3,4,4,5};
-		int k =1;
-		for(int i =1;i<list.length;i++) {
-			if(list[i]!=list[i-1]) {
+		int[] list = { 1, 1, 2, 2, 3, 3, 4, 4, 5 };
+		int k = 1;
+		for (int i = 1; i < list.length; i++) {
+			if (list[i] != list[i - 1]) {
 				list[k] = list[i];
 				k++;
 			}
-			
-	    }
+
+		}
 		System.out.println(k);
-	
-		
-			
+
 	}
+	public static void plusOne() {
+		System.out.println("Enter size :");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Enter Elements : ");
+		for(int i=0;i<size;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int[] newDigit = new int[size];
+		for(int i=0;i<size;i++) {
+			if(i==size-1) {
+				arr[i] += 1;
+			}
+			newDigit[i] = arr[i];
+		}
+		for(int i =0;i<size;i++) {
+		System.out.print(newDigit[i]+",");
+	}
+	}
+	
 
 }
