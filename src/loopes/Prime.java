56cee1c27e1ecdc5f52d3cf63390c94db99eb037
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-21 17:27:30 +0530

    PrimeNumber

--- src/loopes/Prime.java
+++ src/loopes/Prime.java
@@ -3,30 +3,59 @@ package loopes;
 import java.util.Scanner;
 
 public class Prime {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		prime();
+		//prime();
+		printPrime();
 
 	}
+
 	public static void prime() {
-		Scanner sc = new Scanner(System.in);
+
 		System.out.print("Enter Number : ");
 		double num = sc.nextDouble();
-	    int count = 0;
-		for(int i=2;i*i<num;i++) {
-			if(num%i==0) {
+		int count = 0;
+		for (int i = 2; i * i < num; i++) {
+			if (num % i == 0) {
 				count++;
 				break;
 			}
 		}
-		if(count==0) {
+		if (count == 0) {
 			System.out.println("Prime Number");
-		}
-		else {
+		} else {
 			System.out.println("not a Prime");
 		}
-		
-		
+
+	}
+
+	public static void printPrime() {
+		System.out.print("Enter Starting Number : ");
+		int low = sc.nextInt();
+		System.out.print("Enter Last Number : ");
+		int high = sc.nextInt();
+
+		int count = 0;
+		int j;
+		int num = 0;
+		for (int i = low; i <= high; i++) {
+			for (j = 2; j < i; j++) {
+				if (i % j == 0) {
+					count++;
+					break;
+				}
+
+			}
+			if (count==0) {
+				num = i;
+				System.out.println(num);
+
+			}
+
+		}
+		System.out.println(num);
+
 	}
 
 }
