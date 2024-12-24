50305ee9828a793b7b14dd3aeb7d29e87cd9fa2d
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-24 22:11:13 +0530

    Pattern

--- src/practiceOfPattern/StarPattern.java
+++ src/practiceOfPattern/StarPattern.java
@@ -9,64 +9,118 @@ public class StarPattern {
 //		right() ;
 //		reverse();
 //		left();
-		reverse1();
+//		reverse1();
+//		diamond();
+		shortCodeDiamond();
 	}
 
 	public static void right() {
 		System.out.println("Enter a Num :");
 		int n = sc.nextInt();
-		for(int i =1;i<=n;i++) {
-			for(int j = 1;j<=i;j++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print('*');
 			}
 			System.out.println();
 		}
-		
+
 	}
+
 	public static void reverse() {
 		System.out.println("Enter a Num :");
 		int n = sc.nextInt();
-		for(int i =n;i>=1;i--) {
-			for(int j = 1;j<=i;j++) {
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print('*');
 			}
 			System.out.println();
 		}
-		
+
 	}
+
 	public static void left() {
 		System.out.println("Enter a Num :");
 		int n = sc.nextInt();
-		
-		for(int i = 1;i<=n;i++) {
-			for(int j = 1;j<=n;j++) {
-				if(j<=n-i) {
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j <= n - i) {
 					System.out.print("_ ");
-				}
-				else {
+				} else {
 					System.out.print("* ");
 				}
 			}
 			System.out.println();
 		}
 	}
+
 	public static void reverse1() {
 		System.out.println("Enter a Num :");
 		int n = sc.nextInt();
-		
-		for(int i = n;i>=1;i--) {
-			for(int j = 1;j<=n;j++) {
-				if(j<=n-i) {
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= n; j++) {
+				if (j <= n - i) {
 					System.out.print("_ ");
-				}
-				else {
+				} else {
 					System.out.print("* ");
 				}
 			}
 			System.out.println();
 		}
 	}
-	
-	
+
+	public static void diamond() {
+		System.out.println("Enter a Num :");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i == 1 || i == 5) {
+					if (j == 3) {
+						System.out.print("* ");
+					} else {
+						System.out.print("_ ");
+					}
+
+				} else if (i == 2 || i == 4) {
+					if (j == 1 || j == 5) {
+						System.out.print("- ");
+
+					} else {
+						System.out.print("* ");
+					}
+				} else {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	// short code for above pattern
+	public static void shortCodeDiamond() {
+		System.out.println("Enter a Num :");
+		int n = sc.nextInt();
+		int sp = n / 2;
+		int st = 1;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= sp; j++) {
+				System.out.print("_ ");
+			}
+			for (int j = 1; j <= st; j++) {
+				System.out.print("* ");
+			}
+			if (i <= n / 2) {
+				sp--;
+				st += 2;
+			} else {
+				sp++;
+				st -= 2;
+			}
+			System.out.println();
+		}
+	}
 
 }
