9e01257238596e6ea25ab9e7abc6b4b8b7dbcc8c
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2024-12-29 22:40:24 +0530

    Functions

---
+++ src/function/DigitFrequency.java
@@ -0,0 +1,31 @@
+package function;
+
+import java.util.Scanner;
+
+public class DigitFrequency {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Enter a num : ");
+		int n = sc.nextInt();
+		System.out.println("Enter a digit : ");
+		int d = sc.nextInt();
+		int f = digit(n, d);
+		System.out.println(f);
+
+	}
+
+	public static int digit(int n, int d) {
+
+		int count = 0;
+		while (n != 0) {
+			int rem = n % 10;
+			n = n / 10;
+			if (rem == d) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
