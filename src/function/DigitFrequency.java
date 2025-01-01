983603dfeba5c76420aa8eaa5e1dca235b750b09
ChanchalPatelcp <chanchalpatel7389@gmail.com>
2025-01-01 18:48:36 +0530

    NumberSystem

--- src/function/DigitFrequency.java
+++ src/function/DigitFrequency.java
@@ -10,9 +10,12 @@ public class DigitFrequency {
 		int n = sc.nextInt();
 		System.out.println("Enter a digit : ");
 		int d = sc.nextInt();
-		int f = digit(n, d);
-		System.out.println(f);
-
+//		int f = digit(n, d);
+//		System.out.println(f);
+//		int g = decimalToAnyBase(n, d);
+//		System.out.println(g);
+		int h = anyBasetoDecimal(n,d) ;
+		System.out.println(h);
 	}
 
 	public static int digit(int n, int d) {
@@ -27,11 +30,30 @@ public class DigitFrequency {
 		}
 		return count;
 	}
-	public static void numberSystem() {
-		int n = sc.nextInt();
-		for(int i =0;i<n;i++) {
-			
+
+	public static int decimalToAnyBase(int n, int d) {
+		int p = 1;
+		int dtb = 0;
+		while (n != 0) {
+			int rem = n % d;
+			dtb += rem * p;
+			p = p * 10;
+			n = n / d;
 		}
+		return dtb;
+
 	}
+	public static int anyBasetoDecimal(int n, int d) {
+		int p =1;
+		int pow = 0;
+		while(n!=0) {
+			int rem = n%10;
+			 pow += rem * p;
+			p = p*d;
+			n = n / 10;
+		}
+		return pow;
+	}
+	
 
 }
